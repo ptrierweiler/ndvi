1f7117c6d692ae9c39f9dd060348cf13fc741d3b
ptrierweiler <ptrierweiler@gmail.com>
2016-03-26 18:50:22 -0300

    Added raster ingest to db process

--- modis_ndvi_process.py
+++ modis_ndvi_process.py
@@ -57,7 +57,34 @@ for d in doy_tuples:
     gtif_list = []
     for i in os.listdir(homedir):
         if i.split('.')[-1] == 'tif':
-            gtif_list.append(i)
+            gtif_list.append(homedir +'/'+i)
 
-    #Ingetsting images into postgres
-    
+    # Ingetsting images into postgres
+    for tif in gtif_list:
+        schema = os.path.basename(tif).split('.')[0].lower()
+        layer = '.'.join(os.path.basename(tif).split('.')[1:3]).lower()
+        os.system("raster2pgsql -C -I {tif} -d {schema}.{layer} \
+        | psql patrick".format(tif=tif, schema=schema, layer=layer))
+
+    # Seaching for rasters in the db
+    srch_layer = '_'.join(layer.split('_')[0:2])
+    conn = psycopg2.connect("dbname=patrick")
+    cur = conn.cursor()
+    cur.execute("select r_table_name from raster_columns where \
+    r_table_schema = '{schema}' and r_table_name ~ '{srch_layer}'".format(
+    schema = schema, srch_layer = srch_layer))
+    db_list = cur.fetchall()
+    # assigning rasters to variables
+    for i in db_list:
+        if i[0].split('_')[-1] == 'b01':
+            rast1 = i
+        elif i[0].split('_')[-1] == 'b02':
+            rast2 = i
+    # creating ndvi output
+    ndvi = rast1.replace('b01','ndvi')
+    # create ndvi table
+    cur.execute("create table {schema}.{ndvi} \
+    (rid int primary key, rast raster)".format(schema = schema, ndvi = ndvi)
+
+    cur.execute("insert into mod09q1.ndvi(rid,rast) select \
+     1,st_mapalgebra(r1.rast,1,r2.rast,1, 'case when [rast1.val] + [rast2.val] = 0 then Null else ([rast1.val] - [rast2.val])/([rast1.val] + [rast2.val]) end'::text,'32BF'::text,Null) from mod09q1.a2016033_h12v10_sur_refl_b01 as r1, mod09q1.a2016033_h12v10_sur_refl_b02 as r2")
